9ebe1ee4235fff84951d3ddb4ececd199c82e4ac
Ludovic Coquelle <lcoquelle@gmail.com>
2008-06-08 16:21:10 +0800

    Added tools to Seq (map, iter, filter).

--- seq.ml
+++ seq.ml
@@ -1,5 +1,7 @@
 (* Base *)
 
+exception EmptySeq
+
 type 'a t =
     | Nil
     | Cons of 'a * 'a t lazy_t
@@ -11,6 +13,13 @@ let head seq =
     | Cons(h, q) ->
         Some h
 
+let head_exn seq =
+    match seq with
+    | Nil ->
+        raise EmptySeq
+    | Cons(h, q) ->
+        h
+
 let tail seq =
     match seq with
     | Nil ->
@@ -19,7 +28,7 @@ let tail seq =
         Lazy.force q
 
 
-(* Helper *)
+(* Construction helpers *)
 
 let rec of_list lst =
     match lst with
@@ -31,4 +40,41 @@ let rec of_list lst =
 let rec of_serie fn n0 =
     Cons(n0, lazy (of_serie fn (fn n0)))
 
+
+(* Manipulation *)
+
+let rec map fn seq =
+    (* seq is last arg such that forward op can be used *)
+    match head seq with
+    | None ->
+        Nil
+    | Some e ->
+        Cons(fn e, lazy (map fn (tail seq)))
+
+let rec gmap_exn fn seqs =
+    match seqs with
+    | Nil :: _ ->
+        Nil
+    | _ ->
+        let heads = List.map head_exn seqs in
+        Cons(fn heads, lazy (gmap_exn fn (List.map tail seqs)))
+
+let rec iter fn seq =
+    match head seq with
+    | None ->
+        ()
+    | Some e ->
+        let _ = fn e in
+        iter fn (tail seq)
+
+let rec filter pred seq =
+    match head seq with
+    | None ->
+        Nil
+    | Some h ->
+        if pred h
+        then
+            Cons(h, lazy (filter pred (tail seq) ))
+        else
+            filter pred (tail seq)
 
--- test/seqTest.ml
+++ test/seqTest.ml
@@ -38,6 +38,11 @@ let _ = Tests.register "Base: head, tail" (fun () ->
     _cmp_to_list_end s [1;2;]
 )
 
+let _ = Tests.register "Base: head_exn" (fun () ->
+    let s = Seq.Nil in
+    OUnit.assert_raises Seq.EmptySeq (fun () -> Seq.head_exn s)
+)
+
 let _ = Tests.register "Helper: of_list" (fun () ->
     let l = [1;2;3;] in
     let s = Seq.of_list l in
@@ -48,6 +53,42 @@ let _ = Tests.register "Helper: of_serie" (fun () ->
     let fn = fun x -> x + 1 in
     let s = Seq.of_serie fn 0 in
     _cmp_to_list_cont s [0;1;2;3;4;5;]
+)
+
+let _ = Tests.register "map" (fun () ->
+    let fn = fun x -> x + 1 in
+    let s = Seq.of_serie fn 0 in
+    let s2 = Seq.map (fun x -> x * 2) s in
+    _cmp_to_list_cont s2 [0;2;4;6;]
+)
+
+let _ = Tests.register "gmap" (fun () ->
+    let s1 = Seq.of_list [1;2;3;] in
+    let s2 = Seq.of_list [10;20;30;] in
+    let fn heads = match heads with |x::y::[] -> x + y |_ -> failwith "error" in
+    _cmp_to_list_cont
+        (Seq.gmap_exn fn [s1;s2;])
+        [11; 22; 33;];
+    let s3 = Seq.of_list [1;2;3;4;] in
+    let err = Seq.gmap_exn fn [s3;s2;] in
+    OUnit.assert_raises Seq.EmptySeq (fun () ->
+        _cmp_to_list_cont err [11; 22; 33;]
+    )
+)
+
+let _ = Tests.register "iter" (fun () ->
+    let l = [1;2;3;] in
+    let s = Seq.of_list l in
+    let acc = ref [] in
+    let fn = fun x -> acc := x :: !acc in
+    Seq.iter fn s;
+    OUnit.assert_equal !acc (List.rev l)
+)
+
+let _ = Tests.register "filter" (fun () ->
+    let s = Seq.of_list [0;1;2;3;4;5;6;7;8;9;] in
+    let f = Seq.filter (fun x -> x mod 2 = 0) s in
+    _cmp_to_list_end f [0;2;4;6;8;]
 )
 
 let _ = Tests.run "Seq test suite"
